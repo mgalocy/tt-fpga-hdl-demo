71c2903f57afd5b423e8a6f0606b2a92ba71e83e
mgalocy <161381353+mgalocy@users.noreply.github.com>
2024-03-04 14:32:16 -0500

    calculator test

--- src/tt_um_fpga_hdl_demo.v
+++ src/tt_um_fpga_hdl_demo.v
@@ -1,54 +1,194 @@
-`default_nettype none
+\m5_TLV_version 1d: tl-x.org
+\m5
+   use(m5-1.0)
+   
 
-module tt_um_fpga_hdl_demo #( parameter MAX_COUNT = 24'd10_000_000 ) (
+   // #################################################################
+   // #                                                               #
+   // #  Starting-Point Code for MEST Course Tiny Tapeout Calculator  #
+   // #                                                               #
+   // #################################################################
+   
+   // ========
+   // Settings
+   // ========
+   
+   //-------------------------------------------------------
+   // Build Target Configuration
+   //
+   // To build within Makerchip for the FPGA or ASIC:
+   //   o Use first line of file: \m5_TLV_version 1d --inlineGen --noDirectiveComments --noline --clkAlways --bestsv --debugSigsYosys: tl-x.org
+   //   o set(MAKERCHIP, 0)  // (below)
+   //   o For ASIC, set my_design (below) to match the configuration of your repositoy:
+   //       - tt_um_fpga_hdl_demo for tt_fpga_hdl_demo repo
+   //       - tt_um_example for tt06_verilog_template repo
+   //   o var(target, FPGA)  // or ASIC (below)
+   set(MAKERCHIP, 1)   /// 1 for simulating in Makerchip.
+   var(my_design, tt_um_template)   /// The name of your top-level TT module, to match your info.yml.
+   var(target, FPGA)  /// FPGA or ASIC
+   //-------------------------------------------------------
+   
+   var(debounce_inputs, 0)         /// 1: Provide synchronization and debouncing on all input signals.
+                                   /// 0: Don't provide synchronization and debouncing.
+                                   /// m5_neq(m5_MAKERCHIP, 1): Debounce unless in Makerchip.
+   
+   // ======================
+   // Computed From Settings
+   // ======================
+   
+   // If debouncing, a user's module is within a wrapper, so it has a different name.
+   var(user_module_name, m5_if(m5_debounce_inputs, my_design, m5_my_design))
+   var(debounce_cnt, m5_if_eq(m5_MAKERCHIP, 1, 8'h03, 8'hff))
+
+\SV
+   m4_include_lib(https:/['']/raw.githubusercontent.com/efabless/chipcraft---mest-course/main/tlv_lib/calculator_shell_lib.tlv)
+   // Include Tiny Tapeout Lab.
+   m4_include_lib(https:/['']/raw.githubusercontent.com/os-fpga/Virtual-FPGA-Lab/35e36bd144fddd75495d4cbc01c4fc50ac5bde6f/tlv_lib/tiny_tapeout_lib.tlv)
+   m4_include_lib(https:/['']/raw.githubusercontent.com/efabless/chipcraft---mest-course/main/reference_designs/PmodKYPD.tlv)
+
+\TLV calc()
+   
+   |calc
+      m5+PmodKYPD(|calc, /keypad, @0, $ui_in[3:0], 1'b1, ['left: 40, top: 80, width: 200, height: 200'])
+      @0
+         $reset = *reset;
+         $op[2:0] = *ui_in[6:4];
+         
+      @1
+         $val1[7:0] = >>2$out;
+         $val2[7:0] = {4'b0, *ui_in[3:0]};
+         $sum[7:0] = $val1 + $val2;
+         $diff[7:0] = $val1 - $val2;
+         $prod[7:0] = $val1 * $val2;
+         $quot[7:0] = $val1 / $val2;
+         $equals_in = *ui_in[7];
+         
+         $valid = $reset ? 1'b0 : $equals_in && ! >>1$equals_in;
+         $reset_or_valid = $valid || $reset;
+      @2   
+         $mem[7:0] = $reset              ? 8'b0 :
+                     $valid && ($op[2:0] == 3'b101) ? $val1 :
+                                           >>1$mem;
+         
+         $out[7:0] = $reset ? 8'b0 :
+                     !$valid ? >>1$out :
+                     ($op[1:0] == 3'b000) ? $sum :
+                     ($op[1:0] == 3'b001) ? $diff :
+                     ($op[1:0] == 3'b010) ? $prod :
+                     ($op[1:0] == 3'b011) ? $quot :
+                     ($op[1:0] == 3'b100) ? >>2$mem : >>1$out;
+         
+      @3
+         m5+sseg_decoder($segments_n, /keypad$digit_pressed)
+         $digit[3:0] = $out[3:0];
+         
+         *uo_out =
+            $digit == 4'h0
+               ? 8'b00111111 :
+            $digit == 4'h1
+               ? 8'b00000110 :
+            $digit == 4'h2
+               ? 8'b01011011 :
+            $digit == 4'h3
+               ? 8'b01001111 :
+            $digit == 4'h4
+               ? 8'b01100110 :
+            $digit == 4'h5
+               ? 8'b01101101 :
+            $digit == 4'h6
+               ? 8'b01111101 :
+            $digit == 4'h7
+               ? 8'b00000111 :
+            $digit == 4'h8
+               ? 8'b01111111 :
+            $digit == 4'h9
+               ? 8'b01100111 :
+            $digit == 4'hA
+               ? 8'b01110111 :
+            $digit == 4'hB
+               ? 8'b01111100 :
+            $digit == 4'hC
+               ? 8'b00111001 :
+            $digit == 4'hD
+               ? 8'b00111001 :
+            $digit == 4'hE
+               ? 8'b01111001 :
+            $digit == 4'hF
+               ? 8'b01110001 :
+                 8'b10000000 ;
+         
+ 
+   
+   // Note that pipesignals assigned here can be found under /fpga_pins/fpga.
+   
+   
+
+   m5+cal_viz(@2, /fpga)
+   
+   // Connect Tiny Tapeout outputs. Note that uio_ outputs are not available in the Tiny-Tapeout-3-based FPGA boards.
+   *uo_out = 8'b0;
+   m5_if_neq(m5_target, FPGA, ['*uio_out = 8'b0;'])
+   m5_if_neq(m5_target, FPGA, ['*uio_oe = 8'b0;'])
+   
+\SV
+
+// ================================================
+// A simple Makerchip Verilog test bench driving random stimulus.
+// Modify the module contents to your needs.
+// ================================================
+
+module top(input logic clk, input logic reset, input logic [31:0] cyc_cnt, output logic passed, output logic failed);
+   // Tiny tapeout I/O signals.
+   logic [7:0] ui_in, uo_out;
+   m5_if_neq(m5_target, FPGA, ['logic [7:0]uio_in,  uio_out, uio_oe;'])
+   logic [31:0] r;
+   always @(posedge clk) r <= m5_if(m5_MAKERCHIP, ['$urandom()'], ['0']);
+   assign ui_in = r[7:0];
+   m5_if_neq(m5_target, FPGA, ['assign uio_in = 8'b0;'])
+   logic ena = 1'b0;
+   logic rst_n = ! reset;
+   
+   // Instantiate the Tiny Tapeout module.
+   m5_user_module_name tt(.*);
+   
+   assign passed = top.cyc_cnt > 60;
+   assign failed = 1'b0;
+endmodule
+
+
+// Provide a wrapper module to debounce input signals if requested.
+m5_if(m5_debounce_inputs, ['m5_tt_top(m5_my_design)'])
+\SV
+
+
+
+// =======================
+// The Tiny Tapeout module
+// =======================
+
+module m5_user_module_name (
     input  wire [7:0] ui_in,    // Dedicated inputs - connected to the input switches
     output wire [7:0] uo_out,   // Dedicated outputs - connected to the 7 segment display
+    m5_if_eq(m5_target, FPGA, ['/']['*'])   // The FPGA is based on TinyTapeout 3 which has no bidirectional I/Os (vs. TT6 for the ASIC).
+    input  wire [7:0] uio_in,   // IOs: Bidirectional Input path
+    output wire [7:0] uio_out,  // IOs: Bidirectional Output path
+    output wire [7:0] uio_oe,   // IOs: Bidirectional Enable path (active high: 0=input, 1=output)
+    m5_if_eq(m5_target, FPGA, ['*']['/'])
     input  wire       ena,      // will go high when the design is enabled
     input  wire       clk,      // clock
     input  wire       rst_n     // reset_n - low to reset
 );
+   wire reset = ! rst_n;
+   
+\TLV
+   /* verilator lint_off UNOPTFLAT */
+   // Connect Tiny Tapeout I/Os to Virtual FPGA Lab.
+   m5+tt_connections()
+   
+   // Instantiate the Virtual FPGA Lab.
+   m5+board(/top, /fpga, 7, $, , calc)
+   // Label the switch inputs [0..7] (1..8 on the physical switch panel) (top-to-bottom).
+   m5+tt_input_labels_viz(['"Value[0]", "Value[1]", "Value[2]", "Value[3]", "Op[0]", "Op[1]", "Op[2]", "="'])
 
-    wire reset = ! rst_n;
-    wire [6:0] led_out;
-    assign uo_out[6:0] = led_out;
-    assign uo_out[7] = 1'b0;
-
-    // put bottom 8 bits of second counter out on the bidirectional gpio
-    // assign uio_out = second_counter[7:0];
-
-    // external clock is 10MHz, so need 24 bit counter
-    reg [23:0] second_counter;
-    reg [3:0] digit;
-
-    // if external inputs are set then use that as compare count
-    // otherwise use the hard coded MAX_COUNT
-    wire [23:0] compare = ui_in == 0 ? MAX_COUNT: {6'b0, ui_in[7:0], 10'b0};
-
-    always @(posedge clk) begin
-        // if reset, set counter to 0
-        if (reset) begin
-            second_counter <= 0;
-            digit <= 0;
-        end else begin
-            // if up to 16e6
-            if (second_counter == compare) begin
-                // reset
-                second_counter <= 0;
-
-                // increment digit
-                digit <= digit + 1'b1;
-
-                // only count from 0 to 9
-                if (digit == 9)
-                    digit <= 0;
-
-            end else
-                // increment counter
-                second_counter <= second_counter + 1'b1;
-        end
-    end
-
-    // instantiate segment display
-    seg7 seg7(.counter(digit), .segments(led_out));
-
+\SV
 endmodule
